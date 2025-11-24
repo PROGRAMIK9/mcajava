d04dc6fd3337a25f33c95a028a3df60034f13007
Kr1szz <kriszzgfx@gmail.com>
2025-11-24 18:31:26 +0530

    Keyboard input added

--- src/Area.java
+++ src/Area.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
 import shapes.*;
 
 public class Area {
     public static void main(String[] args) throws Exception {
-       Square s=new Square(4);
-       Circle c=new Circle(4);
-       Tri t=new Tri(5, 10);
-       System.out.println("Area of Square: " +s.getArea());
-        System.out.println("Area of Circle: " +c.getArea());
-         System.out.println("Area of Tri: " +t.getArea());
+        Scanner sc = new Scanner(System.in);
+
+        // Square
+        System.out.print("Enter side of Square: ");
+        double side = sc.nextDouble();
+        Square s = new Square(side);
+
+        // Circle
+        System.out.print("Enter radius of Circle: ");
+        double radius = sc.nextDouble();
+        Circle c = new Circle(radius);
+
+        // Triangle
+        System.out.print("Enter base of Triangle: ");
+        double base = sc.nextDouble();
+        System.out.print("Enter height of Triangle: ");
+        double height = sc.nextDouble();
+        Tri t = new Tri(base, height);
+
+        System.out.println("\nArea of Square: " + s.getArea());
+        System.out.println("Area of Circle: " + c.getArea());
+        System.out.println("Area of Triangle: " + t.getArea());
+
+        sc.close();
     }
 }
