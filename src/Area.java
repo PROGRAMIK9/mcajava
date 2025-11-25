19a30b61dce67b32e3a874a513c3563325ab3f39
GopalkrishnaJS <69383830+Kr1szz@users.noreply.github.com>
2025-11-25 12:06:00 +0530

    Update Area.java

--- src/Area.java
+++ src/Area.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import shapes.*;
 
 public class Area {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         // Square
