934c64793564db120802923a130e8710f2b515e0
Aaryamann Prabhu <140898184+PROGRAMIK9@users.noreply.github.com>
2026-03-05 19:46:51 +0530

    P10

---
+++ src/p10/Student.java
@@ -0,0 +1,11 @@
+package com.model;
+
+public class Student {
+	public int id,age;
+	public String name;
+	public Student(int id, String name, int age) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+}
